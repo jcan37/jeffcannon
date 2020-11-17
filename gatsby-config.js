9e8ace67fbcdff73a728fbe70a1c4da8f92e1be1
Jeff Cannon <jeff@radcannon.com>
2020-11-17 16:37:35 -0500

    Update site configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
+    title: `Jeff's Blog`,
+    author: `Jeff Cannon`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://jeffcannon.dev`,
     social: {
-      twitter: `gatsbyjs`,
+      instagram: `jcan37`,
+      twitter: `jcannon37`,
     },
   },
   plugins: [
