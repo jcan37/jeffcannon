c8062e6720d8b98dffa158d11ee5f295708829d3
Jeff Cannon <jeff@radcannon.com>
2022-01-14 15:52:40 -0500

    Add job title

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
       'Backseat Driver',
       'Cactus Massage Therapist',
       'Cat Stair Architect',
+      'Cheese Comedian',
       'Dorsia Reservationist',
       'Financial Wizard',
       'Gimp Suit Model',
